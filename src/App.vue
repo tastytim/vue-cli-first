1782483e0e4b4f0703d0b612eb4730bd6f34d200
tastytim <tastytimgm@gmail.com>
2021-11-10 15:10:22 +0100

    commit init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <Title msg="La mia prima app con Vue CLI!"/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Title from './components/Title.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Title
   }
 }
 </script>
